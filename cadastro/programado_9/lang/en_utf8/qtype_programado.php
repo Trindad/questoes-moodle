b9456bb8f37827828a18a18a5fd9d8e5c8f5ab92
Silvana Trindade <syletri@gmail.com>
2014-07-16 20:30:20 -0300

    Adicionando ajuda para o plugin programado.

---
+++ cadastro/programado_9/lang/en_utf8/qtype_programado.php
@@ -0,0 +1,27 @@
+<?php // $Id: qtype_programado.php,v 1.3 2009/02/26 14:39:44 mjollnir_ Exp $
+/**
+ * The language strings for the Programado question type.
+ *    
+ * @copyright &copy; 2006 Turma Optativa I
+ * @author gttonin@gmail.com
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @package tipo_programado
+ *//** */
+
+$string['addingprogramado'] = 'Adding Programado.';
+$string['editingprogramado'] = 'Editing Programado.';
+$string['programado'] = 'Programado';
+$string['funcao_solucao'] = 'Função para solução do problema';
+$string['formato_resposta'] = 'Formato de resposta';
+$string['regex_programado'] = 'Expressão regular';
+$string['valores_minimo_programado'] = 'Valor mínimo';
+$string['valores_maximo_programado'] = 'Valor máximo';
+$string['formato_resposta_help'] = 'formato de resposta';
+$string['regex_programado_help'] = 'expressão regular';
+$string['valores_minimo_programado_help'] = 'valor mínimo';
+$string['valores_maximo_programado_help'] = 'valor máximo';
+$string['funcao_solucao_help'] = 'função para solução do problema';
+$string['add_parametro'] = 'Adicionar novo parâmetro';
+// TODO add any other requred strings.
+
+?>
