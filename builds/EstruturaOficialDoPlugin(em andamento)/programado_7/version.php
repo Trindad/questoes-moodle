5da17aa65596266ac98c9c7177d2a220a23c0979
Silvana Trindade <syletri@gmail.com>
2014-07-24 20:53:40 -0300

    Estruturando diretórios e adicionando documentação do plugin

--- builds/EstruturaOficialDoPlugin(em andamento)/programado_7/version.php
+++
@@ -1,5 +0,0 @@
-<?PHP // $Id: version.php,v 1.1 2006/08/25 21:35:43 tjhunt Exp $
-
-$plugin->version  = 20140609; // TODO.
-$plugin->requires = 2007101592.05;
-?>
--- testes unitarios/myqtypeidentifier/version.php
+++
@@ -1,5 +0,0 @@
-<?PHP // $Id: version.php,v 1.1 2006/08/25 21:35:43 tjhunt Exp $
-
-$plugin->version  = 2006082500; // TODO.
-$plugin->requires = 2006032200;
-?>
